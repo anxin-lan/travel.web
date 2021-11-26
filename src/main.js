5e876a74891930dd6d245dbf089d2ab5c4ba3192
脉冲星 <8865121+pulsar-lan@user.noreply.gitee.com>
2021-11-27 00:04:21 +0800

    init ready

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+//css
+import './assets/css/normalize.css'
+//引入字体
+import './assets/font/iconfont.css'
+//js(解决移动端延迟300s)
+import FastClick from 'fastclick'
+FastClick.attach(document.body)
+
 new Vue({
   router,
   render: h => h(App)
