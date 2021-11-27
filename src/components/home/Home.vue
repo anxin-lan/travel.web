674bacbf84f9c0a9bc9ca58f6f3f08f836e550a9
脉冲星 <8865121+pulsar-lan@user.noreply.gitee.com>
2021-11-27 12:23:50 +0800

    add Header

---
+++ src/components/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header></Header>
+</template>
+
+<script>
+import Header from './page/Header.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/home/page/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont">&#xe660;</span>
+    </div>
+    <div class="header-search">
+      <span class="iconfont">&#xe693;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      北京
+      <span class="iconfont">&#xe600;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='stylus'>
+@import '~@/assets/css/var.styl';
+
+.header {
+  display: flex;
+  width: 100%;
+  height: 2.75rem;
+  line-height: 2.75rem;
+  // background: #00bcd4;
+  background: $bgColor;
+}
+
+.header-search {
+  flex: 1;
+  width: 12.5rem;
+  height: 1.875rem;
+  line-height: 1.875rem;
+  padding-left: 0.625rem;
+  margin-top: 0.5rem;
+  background: #fff;
+  color: #e4e7ea;
+  border-radius: 0.3125rem;
+}
+
+.header-left {
+  width: 1.875rem;
+  text-align: center;
+  color: $textColor;
+  font-weight: 700;
+}
+
+.header-right {
+  width: 3.125rem;
+  padding-left: 0.3125rem;
+  font-size: 0.875rem;
+  color: $textColor;
+}
+</style>
